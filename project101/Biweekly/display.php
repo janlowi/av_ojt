038fed9031db996cc0c68899777d772b0842e39a
charchar <salgarinocharity@gmail.com>
2024-04-18 16:50:21 +0800

    save

--- project101/Biweekly/display.php
+++ project101/Biweekly/display.php
@@ -7,63 +7,59 @@ include '../Layouts/sidebar.php';
 
 ?>
 
-          <!-- Content wrapper -->
-          <div class="content-wrapper">
-            <!-- Content --> 
-          <!-- Layout container -->
-          <div class="layout-page">
-
-            <div class="container-xxl flex-grow-1 container-p-y">
-              <div class="row">
-                <div class="col-lg-8 mb-4 order-0">
-                  <div class="card">
-                    <div class="d-flex align-items-end row">
-
-                    <table class="table table-success table-striped">
-
-  <thead>
-    <tr>
-      <th scope="col">#</th>
-      <th scope="col">First</th>
-      <th scope="col">Last</th>
-      <th scope="col">Handle</th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr>
-      <th scope="row">1</th>
-      <td>Mark</td>
-      <td>Otto</td>
-      <td>@mdo</td>
-    </tr>
-    <tr>
-      <th scope="row">2</th>
-      <td>Jacob</td>
-      <td>Thornton</td>
-      <td>@fat</td>
-    </tr>
-    <tr>
-      <th scope="row">3</th>
-      <td colspan="2">Larry the Bird</td>
-      <td>@twitter</td>
-    </tr>
-  </tbody>
-
-                    
-                    </table>
-
-
-
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>AV Student Intern Report Form</title>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+         </head>
+         <body class="bg-dark">
+    <div class="container">
+        <div class="row mt-5">
+            <div class="col">
+                <div class="card">
+                 
+                       
                     </div>
-              </div>
-              <!--/ Bootstrap Dark Table -->
+                    <div class="card-body">
+                        <table class="table table-bordered text-center">
+                            <tr class="bg-dark text-white">
+                                <td> Timestamp</td>
+                                <td> Email Address </td>
+                                <td> Name </td>
+                                <td> Assigned Department</td>
+                                <td> Assigned Period Start</td>
+                                <td> Assigned Period End</td>
+                                <td> Summary or Scope of Work</td>
+                                <td> Accomplishments</td>
+                                <td> Challenges</td>
+                                <td> Learning and Conclusion</td>
+                            </tr>
+                            <tr>
+                            <td> 8:18</td>
+                            <td> qwerty@gmail.com</td>
+                            <td> Qwerty</td>
+                            <td> Purchasing</td>
+                            <td> 4/5/24</td>
+                            <td> 4/30/24</td>
+                            <td> szfgsdfjhfgkgjlgujl</td>
+                            <td> zdgsfhfgjkgk</td>
+                            <td> fjfhkfyhll</td>
+                            <td> gkyhlghlfyholfyn</td>
+                            </tr>
+
+
+                        </table>
                     </div>
-                  </div>
                 </div>
-
-
-            <!-- / Content -->
-            <div class="content-backdrop fade"></div>
-          </div>
+            </div>
         </div>
-          <!-- Content wrapper -->
+    </div>
+</body>
+          
