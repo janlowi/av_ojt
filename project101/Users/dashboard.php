d4d88e28d388562a0c6bd1148e4268c5de98665f
marichu <arriesgadomarichu06@gmail.com>
2024-04-19 11:03:12 +0800

    timesheet

---
+++ project101/Users/dashboard.php
@@ -0,0 +1,14 @@
+<?php
+        $servername = "localhost";
+         $username = "root";
+          $password = "";
+           $database = "avojt101";
+        
+    $connect = new mysqli ($servername, $username, $password, $database);
+    if($connect->connect_error) {
+       die("Connection failed: " . $connect->connect_error);
+    } else {
+        // echo "connected successfuly.";
+    }
+
+?>
