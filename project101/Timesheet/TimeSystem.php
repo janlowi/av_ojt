e4f88cf03c0727d176093b03e69171a04e6c9508
marichu <arriesgadomarichu06@gmail.com>
2024-05-07 09:34:11 +0800

    87

--- project101/Timesheet/TimeSystem.php
+++ project101/Timesheet/TimeSystem.php
@@ -1,7 +1,9 @@
 <?php   
-
+if(isset($_SESSION['loggedin']) && $_SESSION['loggedin'] === true){
+    header("location: ../index.php");
+    exit;
+}
 $title="Time Tracking System";
-include '../Php/authenticate.php';
 include '../Layouts/main-user.php'; 
  include '../Php/db_connect.php';
   ?>
@@ -11,7 +13,7 @@ include '../Layouts/main-user.php';
 <?php
 $user_id = $_SESSION['user_id'];
 
-$sql = "SELECT event_type FROM timesheet WHERE DATE(timestamp) = CURDATE() AND user_id = '$user_id' ORDER BY timestamp DESC LIMIT 1";
+$sql = "SELECT event_type FROM timesheet WHERE user_id = '$user_id' ORDER BY timestamp DESC LIMIT 1";
 $query = mysqli_query($connect, $sql);
 
 if ($query && mysqli_num_rows($query) > 0) {
@@ -23,35 +25,29 @@ if ($query && mysqli_num_rows($query) > 0) {
         echo '
         <form action="../Php/time-in-out.php" method="POST">
         <div class="d-grid gap-2">
-            <input type="submit" name="Time_Out" value="Out" class="btn btn-dark">
-            </div>
+            <input type="submit" name="Out" value="Out" class="btn btn-dark">
         </form>
-      
+        </div>
         ';
     } else {
         // If the latest event type is "Out", display the "In" button
         echo '
-       
-        <form action="../Php/time-in-out.php" method="POST">
         <div class="d-grid gap-2">
-            <input type="submit" name="Time_In" value="In" class="btn btn-dark" >
-            </div>
+        <form action="../Php/time-in-out.php" method="POST">
+            <input type="submit" name="In" value="In" class="btn btn-dark" >
         </form>
-      
+        </div>
         ';
     }
-
 } else {
     // If there are no events for the user, display the "In" button by default
     echo '
     <form action="../Php/time-in-out.php" method="POST">
-    <div class="d-grid gap-2">
-
-        <input type="submit" name="Time_In" value="In" class="btn btn-dark">
-        </div>
+        <input type="submit" name="In" value="In" class="btn btn-dark">
     </form>';
 }
 ?>
+
                                         
                                           <div class="card-body text-center">
                                             <h5 class="card-title">Time In/Out System</h5>
@@ -66,6 +62,9 @@ if ($query && mysqli_num_rows($query) > 0) {
                                                         </div>
                                           </div>
 
-          <!-- Show current time -->
-      <script src="../Assets/js/dateTime.js"> </script>
-    <!-- Show current time -->
+
+
+                      <!-- Show current time -->
+
+                      <script src="../Assets/js/dateTime.js"> </script>
+                      <!-- Show current time -->
